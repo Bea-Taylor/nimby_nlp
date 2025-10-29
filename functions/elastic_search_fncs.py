05378d5f017f530fc25bb0522ae7d98ab7b0f7b5
Beatrice Taylor <82382427+Bea-Taylor@users.noreply.github.com>
2025-10-29 11:00:24 +0000

    refactored and added class to esf

--- functions/elastic_search_fncs.py
+++ functions/elastic_search_fncs.py
@@ -18,8 +18,10 @@ class ElasticSearchFncs:
             [f"{self.db_host}:{self.db_port}"],
             basic_auth=(self.db_user, self.db_pass), 
             verify_certs=True,
-            ca_certs='athena_es_full_chain.crt'
+            ca_certs='../athena_es_full_chain.crt'
         )
+
+
 
     def check_connection(self):
         """Check connection to the Elastic Search database."""
@@ -28,12 +30,37 @@ class ElasticSearchFncs:
         else:
             print("Could not connect to Elastic Search")
 
-    def res_units_x_query(self, x_res_units, since_year="01/01/2014", to_year="now"):
+    
+
+    def scroll_and_retrieve(self, query):
+        # Initialise scrolling
+        response = self.es.search(index="applications", body=query, scroll="2m", size=10000)
+        scroll_id = response['_scroll_id']
+        hits = response['hits']['hits']
+        
+        all_hits = []
+        all_hits.extend(hits)
+
+        # Keep scrolling until no hits are returned
+        while len(hits) > 0:
+            response = self.es.scroll(scroll_id=scroll_id, scroll="2m")
+            scroll_id = response['_scroll_id']
+            hits = response['hits']['hits']
+            all_hits.extend(hits)
+
+        # Normalize the results into a DataFrame
+        df = pd.json_normalize(all_hits)
+
+        return df 
+    
+
+
+    def res_units_x_query(self, min_res_units, since_year="01/01/2014", to_year="now"):
         """Elastic Search query to filter applications with a minimum number of proposed residential units.
 
         Args:
             es (obj): Elastic Search object.
-            x_res_units (int): Minimum number of proposed residential units.
+            min_res_units (int): Minimum number of proposed residential units.
             since_year (str, optional): Initial date of the search. Defaults to "01/01/2014".
 
         Returns:
@@ -47,7 +74,7 @@ class ElasticSearchFncs:
                 {
                 "range": {
                     "application_details.residential_details.total_no_proposed_residential_units": {
-                    "gte": x_res_units
+                    "gte": min_res_units
                     }
                 }
                 },
@@ -106,34 +133,18 @@ class ElasticSearchFncs:
         ]
         }
 
-
-        # Initialise scrolling
-        response = self.es.search(index="applications", body=query, scroll="2m", size=10000)
-        scroll_id = response['_scroll_id']
-        hits = response['hits']['hits']
-        
-        all_hits = []
-        all_hits.extend(hits)
-
-        # Keep scrolling until no hits are returned
-        while len(hits) > 0:
-            response = self.es.scroll(scroll_id=scroll_id, scroll="2m")
-            scroll_id = response['_scroll_id']
-            hits = response['hits']['hits']
-            all_hits.extend(hits)
-
-        # Normalize the results into a DataFrame
-        df = pd.json_normalize(all_hits)
+        df = self.scroll_and_retrieve(query)
 
         return df
 
 
-    def social_units_x_query(self, x_res_units, since_year="01/01/2014", to_year="now"):
+
+    def social_units_x_query(self, min_social_units, since_year="01/01/2014", to_year="now"):
         """Elastic Search query to filter applications with a minimum number of proposed social rent units.
 
         Args:
             es (obj): Elastic Search object.
-            x_res_units (int): Minimum number of proposed residential units.
+            min_social_units (int): Minimum number of proposed residential units.
             since_year (str, optional): Initial date of the search. Defaults to "01/01/2014".
 
         Returns:
@@ -147,7 +158,7 @@ class ElasticSearchFncs:
                 {
                 "range": {
                     "application_details.residential_details.total_no_proposed_residential_units_social_rent": { # this line specifies only applications which add council housing  
-                    "gte": x_res_units
+                    "gte": min_social_units
                     }
                 }
                 },
@@ -208,22 +219,6 @@ class ElasticSearchFncs:
         ]
         }
 
-        # Initialise scrolling
-        response = self.es.search(index="applications", body=query, scroll="2m", size=10000)
-        scroll_id = response['_scroll_id']
-        hits = response['hits']['hits']
-        
-        all_hits = []
-        all_hits.extend(hits)
-
-        # Keep scrolling until no hits are returned
-        while len(hits) > 0:
-            response = self.es.scroll(scroll_id=scroll_id, scroll="2m")
-            scroll_id = response['_scroll_id']
-            hits = response['hits']['hits']
-            all_hits.extend(hits)
-
-        # Normalize the results into a DataFrame
-        df = pd.json_normalize(all_hits)
+        df = self.scroll_and_retrieve(query)
 
         return df
