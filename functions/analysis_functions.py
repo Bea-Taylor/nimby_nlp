cdf8594bd2b345a84af863a39b893ef49645268c
Beatrice Taylor <82382427+Bea-Taylor@users.noreply.github.com>
2025-10-29 15:54:05 +0000

    refactored functions

--- functions/analysis_functions.py
+++ functions/analysis_functions.py
@@ -1,8 +1,16 @@
 import regex
 
-def keyword_match(df, column, keyword):
-    pattern = rf'\b{regex.escape(keyword)}s?\b'
-    matched_comments = df[df[column].str.contains(pattern, flags=regex.IGNORECASE, regex=True, na=False)]
-    n = len(matched_comments)
-    print(f"Number of comments mentioning '{keyword}': {n}")
-    return matched_comments
+class AnalysisFunctions:
+
+    def __init__(self):
+        pass
+
+    def keyword_match(df, column, keyword):
+        pattern = rf'\b{regex.escape(keyword)}s?\b'
+        matched_comments = df[df[column].str.contains(pattern, flags=regex.IGNORECASE, regex=True, na=False)]
+        n = len(matched_comments)
+
+        print(f"Number of comments mentioning '{keyword}': {n}")
+        print("Percent of comments mentioning '{keyword}': {:.2f}%".format((n / len(df)) * 100))
+
+        return matched_comments
