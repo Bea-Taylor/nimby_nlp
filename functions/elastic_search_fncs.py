2e0125aa9ed01b29ef210e711e1abfa66799505d
Beatrice Taylor <82382427+Bea-Taylor@users.noreply.github.com>
2025-10-29 14:35:35 +0000

    refactored to reuse common methods

--- functions/elastic_search_fncs.py
+++ functions/elastic_search_fncs.py
@@ -12,6 +12,7 @@ class ElasticSearchFncs:
         self.db_pass = 'odbc_readonly'
         self.db_port = '10099'
         self.db_name = 'gla-ldd-external'
+        self.index = 'applications'
 
         # Creates connection to the dataset
         self.es = Elasticsearch(
@@ -32,9 +33,9 @@ class ElasticSearchFncs:
 
     
 
-    def scroll_and_retrieve(self, query):
+    def _scroll_and_retrieve(self, query):
         # Initialise scrolling
-        response = self.es.search(index="applications", body=query, scroll="2m", size=10000)
+        response = self.es.search(index=self.index, body=query, scroll="2m", size=10000)
         scroll_id = response['_scroll_id']
         hits = response['hits']['hits']
         
@@ -53,6 +54,31 @@ class ElasticSearchFncs:
 
         return df 
     
+
+
+    def _year_bucket_and_retrieve(self, query):
+        # Execute the query
+        response = self.es.search(index=self.index, body=query)
+
+        # Process the response into a DataFrame
+        buckets = response["aggregations"]["applications_by_year"]["buckets"]
+        data = []
+
+        for bucket in buckets:
+            year = bucket["key_as_string"][-4:]  # Extract the year from the timestamp
+            range_data = {}
+            for range_bucket in bucket["unit_ranges"]["buckets"]:
+                range_key = f"{int(range_bucket.get('from', 0))}-{int(range_bucket.get('to', 0))}"
+                range_total_units = range_bucket["total_units"]["value"]
+                range_data[range_key] = range_total_units
+            range_data["year"] = year
+            data.append(range_data)
+
+        # Create a DataFrame, filling missing ranges with 0
+        df = pd.DataFrame(data).fillna(0)
+
+        return df
+
 
 
     def res_units_x_query(self, min_res_units, since_year="01/01/2014", to_year="now"):
@@ -133,7 +159,7 @@ class ElasticSearchFncs:
         ]
         }
 
-        df = self.scroll_and_retrieve(query)
+        df = self._scroll_and_retrieve(query)
 
         return df
 
@@ -219,6 +245,113 @@ class ElasticSearchFncs:
         ]
         }
 
-        df = self.scroll_and_retrieve(query)
+        df = self._scroll_and_retrieve(query)
+
+        return df
+    
+
+
+    def get_approved_units_by_ranges(self, since_year="01/01/2014", to_year="now"):
+        """
+        Get the number of permitted applications and total number of residential units applied for by year.
+        These are grouped into ranges of residential units.
+
+        Args:
+            es (obj): elasticsearch object.
+            index (str, optional): elasticsearch index. Defaults to "applications".
+            since_year (str, optional): Include applications with decision made from this date onwards. Defaults to "01/01/2010".
+
+        Returns:
+            dataframe: Dataframe with the number of applications and total number of residential units by year and range.
+        """
+
+        # aggregation includes the from value and excludes the to value for each range
+        ranges = [
+            {"from": 1, "to": 2},      # 1
+            {"from": 2, "to": 10},     # 2-9
+            {"from": 10, "to": 50},    # 10-49
+            {"from": 50, "to": 100},   # 50-99
+            {"from": 100, "to": 150},  # 100-149
+            {"from": 150, "to": 200},  # 150-199
+            {"from": 200, "to": 250},  # 200-249
+            {"from": 250, "to": 300},  # 250-299
+            {"from": 300, "to": 350},  # 300-349
+            {"from": 350, "to": 400},  # 350-399
+            {"from": 400, "to": 450},  # 400-449
+            {"from": 450, "to": 500},  # 450-499
+            {"from": 500},             # 500+
+        ]
+
+        # Define the query with range aggregations
+        query = {
+            "query": {
+                "bool": {
+                "must": [
+                    {
+                    "range": {
+                        "decision_date": {
+                        "gte": since_year, 
+                        "lte": to_year
+                        }
+                    }
+                    },
+                    {
+                    "range": {
+                        "application_details.residential_details.total_no_proposed_residential_units": {
+                            "gte": 1
+                        }
+                    }
+                    }
+                ],
+                "must_not": [
+                    {
+                    "term": {
+                        "status.keyword": "Superseded"
+                    }
+                    }
+                ],
+                "should": [
+                    {
+                    "query_string": {
+                        "query": "(Approved) OR (Not required) OR (Allowed)",
+                        "fields": ["decision", "status"]
+                    },
+                    },
+                    {
+                    "exists":{
+                        "field" : "actual_completion_date"
+                    }
+                    }
+                ],
+                "minimum_should_match" : 1,
+                }
+            },
+            "size": 0,  # No documents, only aggregation results
+            "aggs": {
+                "applications_by_year": {
+                    "date_histogram": {
+                        "field": "valid_date",
+                        "calendar_interval": "year"
+                    },
+                    "aggs": {
+                        "unit_ranges": {
+                            "range": {
+                                "field": "application_details.residential_details.total_no_proposed_residential_units",
+                                "ranges": ranges
+                            },
+                            "aggs": {
+                                "total_units": {
+                                    "sum": {
+                                        "field": "application_details.residential_details.total_no_proposed_residential_units"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        df = self._year_bucket_and_retrieve(query)
 
         return df
